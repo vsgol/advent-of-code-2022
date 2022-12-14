5f04b858ecac1ea28544da1d25c9d547b06948a7
Valery <galloDest@gmail.com>
2022-12-15 00:48:05 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(): Int {
+        val input = readInput("Day01")
+        val (last, max) = input.fold(0 to 0) { acc, s ->
+            if (s.isEmpty()) {
+                0 to maxOf(acc.second, acc.first)
+            } else {
+                acc.first + s.toInt() to acc.second
+            }
+        }
+        return maxOf(max, last)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(): Int {
+        val input = readInput("Day01")
+        val (last, maxList) = input.fold<String, Pair<Int, MutableList<Int>>>(0 to mutableListOf()) { acc, s ->
+            if (s.isEmpty()) {
+                acc.second.add(acc.first)
+                0 to acc.second
+            } else {
+                acc.first + s.toInt() to acc.second
+            }
+        }
+        maxList.add(last)
+        println(maxList)
+        return maxList.sorted().takeLast(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1())
+    println(part2())
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,12 +5,13 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("inputs", "$name.txt")
     .readLines()
 
 /**
  * Converts string to md5 hash.
  */
+@Suppress("unused")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
@@ -18,4 +19,5 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
+@Suppress("unused")
 fun Any?.println() = println(this)
